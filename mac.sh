a46be390d1d87d333fe4baa0a2807a585f055528
Hamdi Bayhan <hamdibayhan@gmail.com>
2016-10-07 14:36:37 +0300

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -69,7 +69,7 @@ fancy_echo "----"
       eval "$(rbenv init -)"
   fi
 fancy_echo "===="
-  
+  source ~/.zshrc
 fancy_echo "&&&&"
 # fancy_echo "Installing rbenv-gem-rehash so the shell automatically picks up binaries after installing gems with binaries..."
 #  brew install rbenv-gem-rehash
